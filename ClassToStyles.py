ab2153001a039947d8d8cf654a4ab0a8808701d3
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-06 13:21:44 +0800

    Refactor ClassToStyles for better regex and style management
    Fix regex patterns for class and style extraction, and improve style handling.

--- ClassToStyles.py
+++ ClassToStyles.py
@@ -1,18 +1,27 @@
 import re
 
 def ClassToStyles(element, table):
-    classtext = re.search(r'class\s*=\s"([^"=]+)"', element)
+    classtext = re.search(r'class\s*=\s"([^"=]+?)"', element)
     if classtext is None:
         return
-    stylestext = re.search(r'class\s*=\s"([^"=]+)"', element)
+    stylestext = re.search(r'style\s*=\s"([^"=]+?)"', element)
     if sylestext is None:
         return
     classes = classtext.group(1).split()
-    styles = {style[0] : style[1] for style in (style.split(":", 1) for style in stylestext.group(1).split(";"))}
+    styles = {style[0].strip() : style[1].strip() for style in (style.split(":", 1) for style in stylestext.group(1).split(";"))}
     for classname, stylestable in table.items():
         if classname in classes:
             for key, value in stylestable:
                 if styles.has_key(key):
                     continue
                 styles[key] = value
-            classes.remove(classes)
+            classes.remove(classname)
+    newclasstext = " ".join(classes)
+    newstylestext = "; ".join(style)
+    return changeclass(changestyles(element, newstylestext),newclasstext)
+
+def changeclass(element, classtext):
+    return re.sub(r'(?<=class)\s*=\s*"[^"=]+?"', classtext, element)
+
+def changestyles(element, stylestext):
+    return re.sub(r'(?<=style)\s*=\s*"[^"=]+?"', stylestext, element)
