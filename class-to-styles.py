beb5b481b39fc9ea81f912747bc1342196e786ec
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-07 00:04:56 +0800

    Refactor class-to-styles.py for improved handling
    Refactor ClassToStyles and ChangeClass functions to handle None cases and improve style handling. Update regex patterns and logic for style and class manipulation.

--- class-to-styles.py
+++ class-to-styles.py
@@ -3,12 +3,13 @@ import re, json, pywikibot
 def ClassToStyles(element, table):
     classtext = re.search(r'class\s*=\s"([\w\d- ]+?)"', element)
     if classtext is None:
-        return
+        return element
     stylestext = re.search(r'style\s*=\s"([^"=]+?)"', element)
     if sylestext is None:
-        return
+        styles = {}
+    else:
+        styles = {style[0].strip() : style[1].strip() for style in (style.split(":", 1) for style in stylestext.group(1).split(";"))}
     classes = set(classtext.group(1).split())
-    styles = {style[0].strip() : style[1].strip() for style in (style.split(":", 1) for style in stylestext.group(1).split(";"))}
     targetclasses = set(table.keys())
     for classname in (classes & targetclasses):
         stylestable = table[classname]
@@ -19,17 +20,23 @@ def ChangeClass(element, addclasses:set, removeclasses:set, classes:set = None):
     if classes is None:
         classtext = re.search(r'class\s*=\s"([\w\d\- ]+?)"', element)
         if classtext is None:
-            return
-    classes = set(classtext.group(1).split()) - removeclasses
+            classes = {}
+        else:
+            classes = set(classtext.group(1).split())
+    classes = classes - removeclasses
     classes.update(addclasses)
-    return re.sub(r'class\s*=\s*"[\w\d\- ]+?"', f'class="{" ".join(classes)}"', element)
+    element, num = re.subn(r'class\s*=\s*"[\w\d\- ]+?"', f'class="{" ".join(classes)}"', element)
+    if num == 0:
+        element += f' class="{" ".join(classes)}"'
+    return element
 
 def ChangeStyles(element, addstyles:dict = {}, removestyles:iter = [], styles:dict = None, replace:bool = True, returnval:str= "element"):
     if styles is None:
         stylestext = re.search(r'styles\s*=\s"([^"=>\n]+?)"', element)
         if stylestext is None:
-            return
-        styles = {style[0].strip() : style[1].strip() for style in (style.split(":", 1) for style in stylestext.group(1).split(";"))}
+            styles = {}
+        else:
+            styles = {style[0].strip() : style[1].strip() for style in (style.split(":", 1) for style in stylestext.group(1).split(";"))}
     for style in removestyles:
         if styles.has_key(key):
             del styles[style]
@@ -38,7 +45,10 @@ def ChangeStyles(element, addstyles:dict = {}, removestyles:iter = [], styles:di
             continue
         styles[key] = value
     if returnval == "element":
-        return re.sub(r'style\s*=\s*"[^"=>\n]+?"', f'style="{"; ".join(styles)}"', element)
+        element, num = re.subn(r'style\s*=\s*"[^"=>\n]+?"', f'style="{"; ".join(styles)}"', element)
+        if num == 0:
+            element += f' style="{"; ".join(styles)}"'
+        return element
     elif returnval == "table":
         return styles
     else:
@@ -57,5 +67,8 @@ def main():
         return
     for page in pagegenerators.SearchPageGenerator(query, site=site, content=True):
         newcontent = page.text
-        for element in re.findall(r"<(?:div|p|span) [^>\n]+>", page.text):
-            newcontent = ClassToStyles(newcontent, table)
+        for match in re.finditer(r"<(div|p|span) ([^>\n]+)>", page.text):
+            newcontent = newcontent.replace(match.group(), f"<{match.group(1)} {ClassToStyles(match.group(2), table)}>")
+        for match in re.finditer(r"(?:^|\n)\s*{| (.*=.*)", page.text):
+            newcontent = newcontent.replace(match.group(), f"{| {ClassToStyles(match.group(1), table)}")
+        page.text = newcontent
